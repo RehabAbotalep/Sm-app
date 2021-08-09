9dca12969f50ae3a163cf64c46e52240b6358ba7
Rehab <rehab.abotalep@gmail.com>
2021-08-09 16:42:42 +0200

    "User Profile"

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make(1234), // password
+            'password' => Hash::make(1234),
             'remember_token' => Str::random(10),
         ];
     }
